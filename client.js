5d00e38593e6657227723fcd6bf9cf3bf65fb87d
FatemehAmir <f.amirkhanloo64@gmai.com>
2021-01-14 00:04:37 +0000

    new

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '10.0.2.15',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on("connect",() => {
+    console.log(" successful")
+    conn.write("Name: Fatemeh");
+  } )
+  return conn;
+}
+
+console.log('Connecting ...');
+//connect();
+//const connectName = faker.name.findName()
+//connect.write(`setName${connectName}`)
+module.exports = {connect};
